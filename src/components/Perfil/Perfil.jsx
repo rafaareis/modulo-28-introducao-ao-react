e98fd86daa2de500ac1c370e7d7406d0965dfc8d
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-07-07 16:37:33 -0300

    Commit 07072023-1637: Aula 06

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import styles from './perfil.module.css';
+import styles from './perfil.module';
 
 const Perfil = ({ nomeUsuario }) => {
     const [perfila, setPerfila] = useState({});
